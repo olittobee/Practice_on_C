d0df6ddaf9423ff94eefba6db835d4414eba0621
Elijah Bayere Olaiya <99737422+olittobee@users.noreply.github.com>
2023-04-02 00:56:43 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+        char name[20];
+
+        printf("Enter your Name : ");
+        scanf("%s" , &name);
+        printf("Your name is : %s", name);
+        printf("\n");
+        printf("Welcome on board : %s", name);
+
+        return 0;
+
+}
+
